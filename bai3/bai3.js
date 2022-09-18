f8f51adf20ace483630bfb9f37e9387ef3572d5f
Trong Tam <trongtam068@gmail.com>
2022-09-18 16:26:03 +0700

    source Bai 3

---
+++ bai3/bai3.js
@@ -0,0 +1,16 @@
+const x = 5;
+// x = 8;s
+let y = 0;
+
+// console.log(y);
+
+console.log(`Hello Nguyen trong tam '' "" from javascript.com   `);
+
+let q = 9,
+  w = 8,
+  e = 7,
+  r = 6,
+  s = 5;
+console.log("Test string ", "x= ", x, " w = ", w, "r = ", r, "s = ", s);
+
+// console.log(" Test string 2222 :", +"x = " + x + "y= " + y + "z= " + z);
